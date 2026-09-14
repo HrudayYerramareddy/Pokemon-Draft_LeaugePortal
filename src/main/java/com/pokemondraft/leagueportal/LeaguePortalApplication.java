585e24759f078cb8b0636432e7c22a79dc3f2e67
Hruday Yerramareddy <147005074+HrudayYerramareddy@users.noreply.github.com>
2026-09-13 23:09:20 -0400

    Add Spring Boot application entry point

---
+++ src/main/java/com/pokemondraft/leagueportal/LeaguePortalApplication.java
@@ -0,0 +1,11 @@
+package com.pokemondraft.leagueportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaguePortalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeaguePortalApplication.class, args);
+    }
+}
