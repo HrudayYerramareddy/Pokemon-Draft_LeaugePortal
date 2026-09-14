efe7fb66c45089174c715223b81d711cb7c05a7c
Hruday Yerramareddy <147005074+HrudayYerramareddy@users.noreply.github.com>
2026-09-14 13:58:02 -0400

    Support Pokémon pool form synchronization

--- src/main/java/com/pokemondraft/leagueportal/repository/PokemonRepository.java
+++ src/main/java/com/pokemondraft/leagueportal/repository/PokemonRepository.java
@@ -2,4 +2,9 @@ package com.pokemondraft.leagueportal.repository;
 import com.pokemondraft.leagueportal.model.Pokemon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-public interface PokemonRepository extends JpaRepository<Pokemon,Long>{ List<Pokemon> findAllByOrderByNameAsc(); List<Pokemon> findByDraftedFalseOrderByNameAsc(); }
+import java.util.Optional;
+public interface PokemonRepository extends JpaRepository<Pokemon,Long>{
+    List<Pokemon> findAllByOrderByNameAsc();
+    List<Pokemon> findByDraftedFalseOrderByNameAsc();
+    Optional<Pokemon> findByName(String name);
+}
